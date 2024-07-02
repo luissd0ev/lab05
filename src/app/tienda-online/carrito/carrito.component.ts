5c040db193a09407de67fdf2912bbb014e82876e
luissd0ev <luissdev@outlook.com>
2024-07-02 11:42:47 -0600

    se agregaron mas reglas

--- src/app/tienda-online/carrito/carrito.component.ts
+++ src/app/tienda-online/carrito/carrito.component.ts
@@ -39,19 +39,22 @@ export class CarritoComponent implements OnInit {
 
 
   ngOnInit(): void {
+    // Carga el usuario desde localStorage al inicializar el componente
     this.loadUserFromStorage();
+
+    // Si hay un usuario actual, obtiene la información del carrito de compras
     if (this.currentUser) {
       this.fetchShoppingCartInfo(this.currentUser.userId); // Pasar el ID del usuario al método
     }
   }
 
-
+  // Obtiene la información del carrito de compras para un usuario específico
   fetchShoppingCartInfo(idUser: number) {
     this.cartService.fetchShoppingCartInfo(idUser).subscribe({
       next: (result) => {
         console.log('Respuesta exitosa en el carrito, elementos del carrito:');
         console.log(result);
-        this.shoppingCartResponse = result;
+        this.shoppingCartResponse = result; // Almacena la respuesta en la propiedad shoppingCartResponse
       },
       error: (error) => {
         console.log('Respuesta incorrecta del carrito');
@@ -60,21 +63,21 @@ export class CarritoComponent implements OnInit {
     });
   }
 
-
+  // Carga el usuario actual desde localStorage
   loadUserFromStorage(): void {
     const storedUser = localStorage.getItem('currentUser');
     if (storedUser) {
-      this.currentUser = JSON.parse(storedUser);
+      this.currentUser = JSON.parse(storedUser); // Almacena el usuario actual en la propiedad currentUser
       console.log('Usuario actual:', this.currentUser);
     } else {
       console.log('No se encontró información del usuario en localStorage.');
-      this.router.navigate(['/tech-market/login']);
+      this.router.navigate(['/tech-market/login']); // Redirige a la página de login si no hay usuario
     }
   }
 
-
+  // Procesa el pago de la orden
   payOrder() {
-    console.log('ORDENNNN');
+    // Mapea los artículos del carrito a un formato adecuado para la orden de compra
     const articulos = this.shoppingCartResponse?.items.map((articulo) => {
       return {
         idArticulo: articulo.idArticulo,
@@ -82,12 +85,17 @@ export class CarritoComponent implements OnInit {
         cantidad: articulo.cantidad,
       };
     });
+
+    // Crea el objeto de solicitud para la orden de compra
     let ordenCompraRequest = {
-      idUsuario: this.currentUser?.userId ?? 0, // Proporciona un valor predeterminado de 0
-      articulos: articulos && articulos.length > 0 ? articulos : [],
+      idUsuario: this.currentUser?.userId ?? 0, // Proporciona un valor predeterminado de 0 si no hay usuario
+      articulos: articulos && articulos.length > 0 ? articulos : [], // Verifica que haya artículos
     };
+
     console.log('orden a procesar');
     console.log(ordenCompraRequest);
+
+    // Llama al servicio para generar la orden de compra
     this.orderService.generateOrderArticles(ordenCompraRequest).subscribe({
       next: (result) => {
         console.log('FUNCIONO; respuesta');
@@ -98,7 +106,8 @@ export class CarritoComponent implements OnInit {
         console.log(error);
       },
     });
+
     return;
-    this.router.navigate(['/tech-market/orden-compra']);
+    this.router.navigate(['/tech-market/orden-compra']); // Redirige a la página de la orden de compra (esta línea no se ejecuta debido al return anterior)
   }
 }
